85bcc085a6e13e78342ece7c72a0a2d626b317d1
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-05-07 19:04:51 +0300

    added Iter

--- Homework2/FictPlay.m
+++ Homework2/FictPlay.m
@@ -1,14 +1,26 @@
 [p1,p2,V]=FictPlay(A,P0,m0,Iter)
     M = size(A)
-    if (P0==1)
-        [M,I] = max(A(mo,:))
-        p1 = A(mo,I);
-        p2 = -A(mo,I);
-    else
-        [M,I] = max(A(:,mo))
-        p1 = A(I,mo);
-        p2 = -A(I,mo);
+    
+    next = -1;
+    if (P0 == 2)
+        next = 1;
+    elseif(P0 == 1)
+        next = 2;
     endif
     
-
+    for i = 1:Iter
+        if (next==1)
+            [M,I] = max(A(mo,:))
+            p1 = A(mo,I);
+            p2 = -A(mo,I);
+            next = 2;    %end of turn
+        elseif
+            [M,I] = max(A(:,mo))
+            p1 = A(I,mo);
+            p2 = -A(I,mo);
+            next = 1;    %end of turn
+        else
+            disp ("Error")    
+        endif
+    endfor
 endfunction
