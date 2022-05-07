2000e19371fd963a6801c426166cfef386e67589
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-05-07 19:29:59 +0300

    Update FictPlay.m

--- Homework2/FictPlay.m
+++ Homework2/FictPlay.m
@@ -1,5 +1,4 @@
 function [p1,p2,V]=FictPlay(A,P0,m0,Iter)
-    M = size(A)
     
     next = -1;
     if (P0 == 2)
@@ -15,7 +14,7 @@ function [p1,p2,V]=FictPlay(A,P0,m0,Iter)
             p2 = -A(m0,I);
             next = 2;    %end of turn
         elseif (next==2)
-            [M,I] = max(A(:,m0))
+            [M,I] = min(A(:,m0))
             p1 = A(I,m0);
             p2 = -A(I,m0);
             next = 1;    %end of turn
