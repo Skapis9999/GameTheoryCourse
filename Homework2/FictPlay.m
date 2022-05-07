88ee470836ba7b947580bc6d11a74b9d0f1d087f
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-05-07 19:06:42 +0300

    Update FictPlay.m

--- Homework2/FictPlay.m
+++ Homework2/FictPlay.m
@@ -10,14 +10,14 @@ function [p1,p2,V]=FictPlay(A,P0,m0,Iter)
     
     for i = 1:Iter
         if (next==1)
-            [M,I] = max(A(mo,:))
-            p1 = A(mo,I);
-            p2 = -A(mo,I);
+            [M,I] = max(A(m0,:))
+            p1 = A(m0,I);
+            p2 = -A(m0,I);
             next = 2;    %end of turn
         elseif (next==2)
-            [M,I] = max(A(:,mo))
-            p1 = A(I,mo);
-            p2 = -A(I,mo);
+            [M,I] = max(A(:,m0))
+            p1 = A(I,m0);
+            p2 = -A(I,m0);
             next = 1;    %end of turn
         else
             disp ("Error")    
