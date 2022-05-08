f535c354ab50535cb9a4256ae25a3e0bbde30192
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-05-08 12:11:15 +0300

    fixed bug

--- Homework2/FictPlay.m
+++ Homework2/FictPlay.m
@@ -1,4 +1,8 @@
 function [p1,p2,V]=FictPlay(A,P0,m0,Iter)
+    % Init
+    [M1,M2]=size(A1);
+    p1 = zeros(M1, 1)';
+    p2 = zeros(M2, 1)';
     
     next = -1;
     if (P0 == 2)
@@ -10,17 +14,19 @@ function [p1,p2,V]=FictPlay(A,P0,m0,Iter)
     
     for i = 1:Iter
         if (next==1)
+            p1 = zeros(M1, 1)';
             [M,I] = max(A(previousMove,:));
-            p1 = A(previousMove,I);
-            p2 = -A(previousMove,I);
+            V = A(previousMove,I);
             next = 2;    %end of turn
             previousMove = I;
+            p1(I) = 1;
         elseif (next==2)
+            p2 = zeros(M2, 1)';
             [M,I] = min(A(:,previousMove));
-            p1 = A(I,previousMove);
-            p2 = -A(I,previousMove);
+            V = A(I,previousMove);
             next = 1;    %end of turn
             previousMove = I;
+            p2(I) = 1;
         else
             disp ("Error")    
         endif
