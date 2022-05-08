5660eeabd3c95146b3e15df8c981df49546d17a0
Christos Trifinopoulos <48195351+Trifi-lit@users.noreply.github.com>
2022-05-08 22:32:24 +0300

    Update SplitEuros.m

--- Homework2/SplitEuros.m
+++ Homework2/SplitEuros.m
@@ -1,6 +1,6 @@
 function [A1,A2,P]=SplitEuros(K)
   A1=zeros(K,K);
-  A1=zeros(K,K);
+  A2=zeros(K,K);
   for i=1:K
     for j=1:K
       if (i+j <= K)
@@ -12,4 +12,4 @@ function [A1,A2,P]=SplitEuros(K)
   [p1,V1,p2,V2] = FictPlayB(A1,A2,1,1,10);
   %[p1,V1,p2,V2] = Nash(A1,A2); %for non-pure strategies
   P=[p1;p2];
-endfunction
+  endfunction
