606b6af3d3bc949f029a6521152df4b67b6818b4
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-05-08 17:58:22 +0300

    fixed bug

--- Homework2/FictPlayB.m
+++ Homework2/FictPlayB.m
@@ -15,15 +15,17 @@ function [p1,V1,p2,V2]=FictPlayB(A1,A2,P0,m0,Iter)
     for i = 1:Iter
         if (next==1)
             p1 = zeros(M1, 1)';
-            [M,I] = max(A1(previousMove,:));
-            V1 = A1(previousMove,I);
+            [M,I] = max(A1(:,previousMove));
+            V1 = A1(I,previousMove);
+            V2 = A2(I,previousMove);
             next = 2;    %end of turn
             previousMove = I;
             p1(I) = 1;
         elseif (next==2)
             p2 = zeros(M2, 1)';
-            [M,I] = min(A2(:,previousMove));
-            V2 = A2(I,previousMove);
+            [M,I] = max(A2(previousMove,:));
+            V1 = A1(previousMove,I);
+            V2 = A2(previousMove,I);
             next = 1;    %end of turn
             previousMove = I;
             p2(I) = 1;
