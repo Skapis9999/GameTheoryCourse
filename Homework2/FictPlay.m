229be3328b0e5f9c91d47a35d872474206e3ed4a
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-05-08 18:30:43 +0300

    Update FictPlay.m

--- Homework2/FictPlay.m
+++ Homework2/FictPlay.m
@@ -15,15 +15,15 @@ function [p1,p2,V]=FictPlay(A,P0,m0,Iter)
     for i = 1:Iter
         if (next==1)
             p1 = zeros(M1, 1)';
-            [M,I] = max(A(previousMove,:));
-            V = A(previousMove,I);
+            [M,I] = max(A(:,previousMove));
+            V = A(I,previousMove);
             next = 2;    %end of turn
             previousMove = I;
             p1(I) = 1;
         elseif (next==2)
             p2 = zeros(M2, 1)';
-            [M,I] = min(A(:,previousMove));
-            V = A(I,previousMove);
+            [M,I] = min(A(previousMove,:));
+            V = A(previousMove,I);
             next = 1;    %end of turn
             previousMove = I;
             p2(I) = 1;
