68db2cbe6a3b4db6b08d78df036cd47a7da7ece9
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-05-25 17:22:18 +0300

    tiny changes

--- Homework3/Question1/HW3_1.m
+++ Homework3/Question1/HW3_1.m
@@ -3,16 +3,17 @@ clear all
 
 % figure(1)
 % fsurf(cost1(x1),[0 5 0 5])
-% 
+%
 % figure(2)
 % fsurf(cost2(x2),[0 5 0 5])
-% 
+%
 % x=x1+x2;
-% 
+%
 % figure(3)
 % fsurf(price(x),[0 5 0 5])
 history1 = [0];
 history2 = [0];
+
 
 x1 = 1;
 x2 = 1;
@@ -23,25 +24,26 @@ mode2 = 1;
 lastProfit1 = 0;
 lastProfit2 = 0;
 
-for i = 1:100
-    if(mode1 == 1)
-        x1=x1+1;
-    end
-    if(mode2 == 1)
-        x2=x2+1;
-    end      
-    x=product(x1,x2);
-    p=price(x);
-    profit1 = x*p - x*cost1(x1);
-    profit2 = x*p - x*cost2(x2);
-    if(profit1 < lastProfit1)
-        mode1=2;
-        x1=x1;
-    end
-    if(profit2 < lastProfit2)
-        mode2=2;
-        x2=x2;
-    end
+for i = 1:30
+        if(mode1 == 1)
+            x1=x1+1;
+        end
+        if(mode2 == 1)
+            x2=x2+1;
+        end
+        x=product(x1,x2);
+        p=price(x);
+        profit1 = x*p - x*cost1(x1);
+        profit2 = x*p - x*cost2(x2);
+        
+        if(profit1 < lastProfit1)
+            mode1=2;
+            x1=x1;
+        end
+        if(profit2 < lastProfit2)
+            mode2=2;
+            x2=x2;
+        end
     lastProfit1 = profit1;
     lastProfit2 = profit2;
     history1 = [history1,x1];
@@ -49,6 +51,7 @@ for i = 1:100
     x1
     x2
 end
+
 
 l = size(history1);
 x = 1:1:l(2);
