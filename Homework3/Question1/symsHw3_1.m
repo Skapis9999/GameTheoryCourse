b678037bdc770c9e1bd22603f14c6a57c804ba6f
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-05-29 23:36:08 +0300

    Add files via upload

---
+++ Homework3/Question1/symsHw3_1.m
@@ -0,0 +1,25 @@
+clear all
+syms x1 x2
+% assume(x1,{'positive','integer'})
+% assume(x2,{'positive','integer'})
+
+x = x1 + x2;
+profit1 = x1*(100 - 2*sqrt(x1+x2)) - (x1+10);
+profit2 = x2*(100 - 2*sqrt(x1+x2)) - (2*x2+5);
+
+dx1 = diff(profit1,x1)
+dx2 = diff(profit2,x2)
+
+eqns = [dx1,dx2];
+S = solve(eqns,[x1 x2]);
+
+A =[double(S.x1)';double(S.x2)']
+
+% subs(dx1, [x1 x2], [795.8800 756.4800]);
+% subs(dx1, [x1 x2], [796 756]);
+% subs(dx2, [x1 x2], [796 756]);
+
+J=jacobian([profit1 profit2],[x1,x2]);
+j1 = subs(J,[x1,x2],[A(1),A(2)])
+
+%negative eigen values
