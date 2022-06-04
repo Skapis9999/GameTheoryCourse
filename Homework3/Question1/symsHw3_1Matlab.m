f95ca62bfdce6c3d5ea547bc1c44e1e6b6194a85
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-06-04 17:45:15 +0300

    Update and rename symsHw3_1.m to symsHw3_1Matlab.m

--- Homework3/Question1/symsHw3_1Matlab.m
+++ Homework3/Question1/symsHw3_1Matlab.m
@@ -1,14 +1,12 @@
 clear all
 syms x1 x2
-% assume(x1,{'positive','integer'})
-% assume(x2,{'positive','integer'})
 
 x = x1 + x2;
 profit1 = x1*(100 - 2*sqrt(x1+x2)) - (x1+10);
 profit2 = x2*(100 - 2*sqrt(x1+x2)) - (2*x2+5);
 
-dx1 = diff(profit1,x1)
-dx2 = diff(profit2,x2)
+dx1 = diff(profit1,x1);
+dx2 = diff(profit2,x2);
 
 eqns = [dx1,dx2];
 S = solve(eqns,[x1 x2]);
@@ -20,6 +18,10 @@ A =[double(S.x1)';double(S.x2)']
 % subs(dx2, [x1 x2], [796 756]);
 
 J=jacobian([profit1 profit2],[x1,x2]);
-j1 = subs(J,[x1,x2],[A(1),A(2)])
-
+j1 = subs(J,[x1,x2],[A(1),A(2)]);
 %negative eigen values
+
+p1 = subs(profit1, [x1,x2], [795.8800 756.4800]);
+p2 = subs(profit2, [x1,x2], [795.8800 756.4800]);
+p1 = subs(profit1, [x1,x2], [796 756]);
+p2 = subs(profit2, [x1,x2], [796 756]);
