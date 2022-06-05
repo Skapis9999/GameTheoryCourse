5b04cabcbc937d63dea5f3ce0cd7acc1dcd69db5
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-06-05 17:59:31 +0300

    Add files via upload

--- Homework3/Exercise4/BackInd.m
+++ Homework3/Exercise4/BackInd.m
@@ -1,20 +1,41 @@
-function U = BackInd (1,U0,Tr,O)
+function U = BackInd (p,U0,Tr,O)
 s=size(Tr,1);
 U=zeros(s,1);
 
+
+% a and b are the probabillities to shoot and not to shoot
+a=1/2;
+b=1/2;
+
 for j=1:s
-  if (U0(j)) #for final states we have fixed payoff
-    U(j)=U0(j)
+  if (U0(j))                  #for final states we have fixed payoff
+    if(O(j-1)==p)
+      U(j)=U0(j)
+    else
+      U(j)=-U0(j)
+    endif
+    endif
 endfor
 
-for j=s:-1:1 #start from final node and update step by step
-  max=0;
-  if (!U0(j)) #for non-final states we need to examine child-nodes
-    for t=1:s
-      if(Tr(t)==j&&U(t)>max) #nodes that have our current node as a parent
-        max=U(t)  #update the maximum payoff for the current node
-      endif
-    endfor
-    U(j)=max;
+for j=s:-1:1                  #start from final node and update step by step
+##  max=0;
+##  if (!U0(j))                 #for non-final states we need to examine child-nodes
+##    for t=1:s
+##      if(Tr(t)==j&&U(t)>max)  #nodes that have our current node as a parent
+##        max=U(t)              #update the maximum payoff for the current node
+##      endif
+##    endfor
+##    endif
+##    U(j)=max+1;
+  if (!U0(j))
+    if(j==s-1)
+      U(j)=U(j+1)
+    else
+      U(j)=a*U(j+1)+b*U(j+2)
+    endif
+
+  endif
+
+
 endfor
 endfunction
--- Homework3/Exercise4/DuelTree.m
+++ Homework3/Exercise4/DuelTree.m
@@ -1,6 +1,6 @@
 function [Tr,O,U0]=DuelTree(D,PH1,PH2)
 #D=10;
-k=2
+k=2;
 
 O=zeros(2*D,1);
 Tr=zeros(2*D,1);
@@ -8,7 +8,6 @@ U0=zeros(2*D,1);
 O(1)=1;
 
 for j=2:2*D
-  j
    odd=mod (j, 2);
    Tr(j)=j-1-odd ;
    if(!odd)
@@ -16,10 +15,6 @@ for j=2:2*D
      if(O(j-1)==1)        #the one who played at the last odd vertice shoots
        U0(j)=PH1(D-j/2+1);
      else
-
-       -j/2;
-       j;
-       D-j/2+1;
        U0(j)=PH2(D-j/2+1);
      endif
    else                    #odd vertices represent the non-final vertices
