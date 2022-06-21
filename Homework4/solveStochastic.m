b8c3f535633f268cbf892b4b081294d9899732e6
Andriotis-Dimitris <104973514+Andriotis-Dimitris@users.noreply.github.com>
2022-06-21 10:33:57 +0300

    Update solveStochastic.m

--- Homework4/solveStochastic.m
+++ Homework4/solveStochastic.m
@@ -1,11 +1,9 @@
 function P=solveStochastic(q1,q2,q3,p1,p2,p3,gamma,T,s0)
-  v1=findVal(q1)
+  v1=findVal(q1);
   v2=findVal(q2);
   v3=0;
   for i=1:4
-    q1(i)=q1(i)+(gamma*(p1(i,1)*v1+p1(i,2)*v2)); 
-  endfor
-  for i=1:4
+    q1(i)=q1(i)+(gamma*(p1(i,1)*v1+p1(i,2)*v2));
     q2(i)=q2(i)+(gamma*(p2(i,1)*v2+p2(i,2)*v2));
   endfor
   T=T-1;
