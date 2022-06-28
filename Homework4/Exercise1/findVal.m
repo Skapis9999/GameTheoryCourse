fe616c26a11e85b9dcce1f8c54b3fbd126a5604b
dimimali <84896475+dimimali@users.noreply.github.com>
2022-06-28 21:47:56 +0300

    Add files via upload

--- Homework4/Exercise1/findVal.m
+++ Homework4/Exercise1/findVal.m
@@ -1,19 +1,7 @@
 function v = findVal (q)
-  if(((q(1)- q(2))*(q(4) - q(3))) < 0)
-    if (q(1)>=q(3)&&q(1)<=q(2))
-      v=q(1);
-    elseif (q(2)>=q(4)&&q(2)<=q(1))
-      v=q(2);
-    elseif (q(3)>=q(1)&&q(3)<=q(4))
-      v=q(3);
-    elseif (q(4)>=q(2)&&q(4)<=q(3))
-      v=q(4);
-    endif
-  else
     a=(q(1)*q(4))-(q(2)*q(3));
     b=(q(1)-q(2))+(q(4)-q(3));
     v=a/b;
-   endif
 %the first part implements a simple minmax algorithm to find a pure equilibrium
 %and the second one checks for non-pure strategies 
-endfunction
+end
