c9a19ebc04a424a15230ad3c329f6b0667a08d7d
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-06-29 14:00:45 +0300

    Add files via upload

---
+++ Homework4/Exercise1/demo1.m
@@ -0,0 +1,15 @@
+﻿pkg load symbolic
+
+x = 2;
+switch (x)
+case 1
+    gamma=0.9;
+case 2
+    gamma =1;
+otherwise
+    "Never gonna give you up. Never gonna let you down. Never gonna run around and desert you. Never gonna make you cry. Never gonna say goodbye. Never gonna tell a lie and hurt you"
+endswitch
+
+stochastic (gamma)
+
+    
